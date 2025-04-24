adc38a6a5f146c373234a1799cfd0966e7c2879d
valverde.cralv <valverde.cralv24@cordoba.salesianos.edu>
2025-04-24 20:31:18 +0200

    Este commit corresponde al error en el bucle del cálculo de la nota media en el caso de que las sumas de las notas o el número de notas sean 0

--- src/GestorEstudiantes.java
+++ src/GestorEstudiantes.java
@@ -10,7 +10,12 @@ public class GestorEstudiantes {
         for (int i = 0; i < estudiante.getNotas().length; i++) { // Error: índice fuera de rango
             suma += estudiante.getNotas()[i];
         }
-        return suma / estudiante.getNotas().length; // Error si el array está vacío
+        if (estudiante.getNotas().length>0) {
+            return suma / estudiante.getNotas().length; // Error si el array está vacío
+        }
+        else {
+            return 0;
+        }
     }
 
     // Encuentra al estudiante con la mejor nota media
