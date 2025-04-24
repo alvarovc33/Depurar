80a591041e104f49faac183f0ecdff3037b74524
valverde.cralv <valverde.cralv24@cordoba.salesianos.edu>
2025-04-24 20:29:58 +0200

    Revert "Este commit corresponde al error en el bucle del cálculo de la nota media en el caso de que las sumas de las notas o el número de notas sean 0"
    This reverts commit c00dda2b1825e834f53906feac793bc367482768.

--- src/GestorEstudiantes.java
+++ src/GestorEstudiantes.java
@@ -10,11 +10,7 @@ public class GestorEstudiantes {
         for (int i = 0; i < estudiante.getNotas().length; i++) { // Error: índice fuera de rango
             suma += estudiante.getNotas()[i];
         }
-        if (estudiante.getNotas().length>0) {
-            return suma / estudiante.getNotas().length; // Error si el array está vacío
-        } else {
-            return 0;
-        }
+        return suma / estudiante.getNotas().length; // Error si el array está vacío
     }
 
     // Encuentra al estudiante con la mejor nota media
